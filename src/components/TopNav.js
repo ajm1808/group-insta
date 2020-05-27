769eedbb4c96a74e251e5533c88190209c940988
ajm1808 <ajm180885@gmail.com>
2020-05-27 12:05:24 +0100

    added src

---
+++ src/components/TopNav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TopNav.css';
+
+const TopNav =()=>{
+    return(
+        <nav>    
+            <ul className="topnav">
+                <li id="insta logo">Instagram</li>
+                <li id="messages">Messages</li>
+            </ul>
+        </nav>
+        
+    )
+
+}
+
+export default TopNav;
