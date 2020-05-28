af18ada6dd26176bd69407e0adfe87f7cc41c962
Richard Wardingley <richardwardingley@hotmail.com>
2020-05-28 14:03:00 +0100

    topnav update

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import './TopNav.css';
+import instalogo from '../img/insta-logo.svg';
+import home from '../img/home.svg';
+import heart from '../img/heart.svg';
+import paperplane from '../img/paper-plane.svg';
+
 
 const TopNav =()=>{
     return(
-        <nav>    
-            <ul className="topnav">
-                <li id="instaLogo">Instagram</li>
-                <li id="search">Search</li>
-                <li id="home">Home</li>
-                <li id="messages">Messages</li>
+  
+        <nav className="topnav">
+            <img alt="Instagram" id="instaLogo" src={instalogo} />
+            <input type="text" id="topSearch" placeholder="Search"></input>
+            <div id="navIcons">
+                <a href="">
+                {/* <img src={home} /> */}
+                    <object type="image/svg+xml" data={home} className='topIcons'></object>
+                </a>
+                <a href="">
+                {/* <img src={paperplane} /> */}
+                    <object type="image/svg+xml" data={paperplane} className='topIcons'></object>
+                </a>
+                <a href="">
+                {/* <img src={paperplane} /> */}
+                    <object type="image/svg+xml" data={paperplane} className='topIcons'></object>
+                </a>
+                <a href="">
+                    {/* <img src={heart} /> */}
+                    <object type="image/svg+xml" data={heart} className='topIcons'></object>
+                </a>
+                <a href="">
+                    {/* <img src={heart} /> */}
+                    <object type="image/svg+xml" data={heart} className='topIcons'></object>
+                </a>
+
+
+                
+                {/* <li id="messages">Messages</li>
                 <li id="discover">Discover</li>
                 <li id="activity">Activity</li>
-                <li id="yourProfile">Your profile</li>
-            </ul>
+                <li id="yourProfile">Your profile</li> */}
+            </div>
+            
         </nav>
         
     )
