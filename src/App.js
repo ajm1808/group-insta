5c966ad140944da72214eb12fbd6f387ae8901ce
ajm1808 <ajm180885@gmail.com>
2020-06-01 09:48:05 +0100

    nothing

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         description: "ILorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
         likes: 0,
       }
-
+      
     ]
 
   }
